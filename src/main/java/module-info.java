38c4bb3be789172de15ebfe55d3855c0e9eddfc1
Seiryu-Kyo <kyo.seiryu@cloud-ace.jp>
2025-11-22 03:08:52 +0900

    SSL通信の暗号モジュールを追加

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module com.kyopan_pan.ytdownloader {
     requires javafx.controls;
     requires javafx.fxml;
 
-
+    // SSL通信（GitHubからのダウンロード）のために必要
+    requires jdk.crypto.ec;
+    
     opens com.kyopan_pan.ytdownloader to javafx.fxml;
     exports com.kyopan_pan.ytdownloader;
 }
